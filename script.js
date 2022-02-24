a05cda39bf14c878af8ae257f00f71d173171a26
santhosh <santhosh.badhri@cilalabs.com>
2022-02-24 17:42:52 +0530

    additions

--- script.js
+++ script.js
@@ -1,80 +1,35 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2FudHljaGludHUiLCJhIjoiY2t4cjlmc2I1MGp5cDJubnludjIzeXR4ZSJ9.K-1gTtD5FCdxJNemZS6t_Q';
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/light-v10',
-        center: [-83.1309121, 39.9831302],
-        zoom: 4
-    });
+let p1, p2;
 
-    map.on('load', () => {
-        // Add an image to use as a custom marker
-        map.loadImage(
-            'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
-            (error, image) => {
-                if (error) throw error;
-                map.addImage('custom-marker', image);
-                // Add a GeoJSON source with 2 points
-                map.addSource('points', {
-                    'type': 'geojson',
-                    'data': {
-                        'type': 'FeatureCollection',
-                        'features': [
-                            {
-                                // feature for Mapbox DC
-                            'type': 'Feature',
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [
-                -75.1089296, 39.9767948
-                                    ]
-                                },
-                                'properties': {
-                                    'title': 'Port Richmond'
-                                }
-                            },
-                            {
-                                // feature for Mapbox SF
-                                'type': 'Feature',
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-75.1835636, 37.776]
-                                },
-                                'properties': {
-                                    'title': 'Strawberry Mansion'
-                                },
-                            },
-                            {
-                                // feature for Mapbox SF
-                                'type': 'Feature',
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-75.1315517, 40.0172382]
-                                },
-                                'properties': {
-                                    'title': 'Southwest Philadelphia'
-                                }
-                            }
-                        ]
-                    }
-                });
+$('#clic').click(
+function tin(){
+  p1 = document.getElementById('point1').value;
+  p2 = document.getElementById('point2').value;
+  var poin1 = {
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Point",
+    "coordinates": [p1]
+  }
+};
+var poin2 = {
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Point",
+    "coordinates": [p2]
+  }
+};
+var units = "miles";
+  var points = {
+  "type": "FeatureCollection",
+  "features": [poin1, poin2]
+};
 
-                // Add a symbol layer
-                map.addLayer({
-                    'id': 'points',
-                    'type': 'symbol',
-                    'source': 'points',
-                    'layout': {
-                        'icon-image': 'custom-marker',
-                        // get the title name from the source's "title" property
-                        'text-field': ['get', 'title'],
-                        'text-font': [
-                            'Open Sans Semibold',
-                            'Arial Unicode MS Bold'
-                        ],
-                        'text-offset': [0, 1.25],
-                        'text-anchor': 'top'
-                    }
-                });
-            }
-        );
-    });
+//=points
+
+var distance = turf.distance([78.5265468,17.413502], [78.5090268,17.3960607], units);
+  document.getElementById('output').inneHTML = distance;
+  console.log(distance.toFixed(1) + 'miles');
+}
+)
